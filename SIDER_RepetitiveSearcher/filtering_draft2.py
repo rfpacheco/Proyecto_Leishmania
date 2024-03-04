84bece988de5ce5420ed1ee8eaa982d6b1326ee4
rfpacheco <ronnyfph@gmail.com>
2024-03-04 16:55:08 +0100

    Added some little modifications and started the script with 5 cromosomes subset

--- SIDER_RepetitiveSearcher/filtering_draft2.py
+++ SIDER_RepetitiveSearcher/filtering_draft2.py
@@ -3,10 +3,10 @@ from modules.blaster import blastn_dic, blastn_blaster, repetitive_blaster
 from modules.files_manager import folder_creator
 
 repetitive_blaster("/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/L_infantum_1-to-5Chr.fasta",
-                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/Results/First_Blaster.csv",
-                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/Results",
+                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/First_Blaster.csv",
+                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/Results100/",
                    "LinJ",
                    600,
-                   0,
-                   1
+                   1,
+                   100                  
 )
