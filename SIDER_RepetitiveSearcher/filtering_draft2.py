67a06adbfaa3c51ad755c282022c5b1608af0e84
rfpacheco <ronnyfph@gmail.com>
2024-03-04 13:58:43 +0100

    Added .py and .ipynb of some tests

---
+++ SIDER_RepetitiveSearcher/filtering_draft2.py
@@ -0,0 +1,12 @@
+from extra.useful import fasta_extractor
+from modules.blaster import blastn_dic, blastn_blaster, repetitive_blaster
+from modules.files_manager import folder_creator
+
+repetitive_blaster("/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/L_infantum_1-to-5Chr.fasta",
+                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/Results/First_Blaster.csv",
+                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/Results",
+                   "LinJ",
+                   600,
+                   0,
+                   1
+)
