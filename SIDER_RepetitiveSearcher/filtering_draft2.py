67f0a6526b2185c314fbb3de256b67d963f5f364
rfpacheco <ronnyfph@gmail.com>
2024-03-13 10:48:20 +0100

    Modified version of filtering_draft2.py with Test3

--- SIDER_RepetitiveSearcher/filtering_draft2.py
+++ SIDER_RepetitiveSearcher/filtering_draft2.py
@@ -2,11 +2,11 @@ from extra.useful import fasta_extractor
 from modules.blaster import blastn_dic, blastn_blaster, repetitive_blaster
 from modules.files_manager import folder_creator
 
-repetitive_blaster("/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/L_infantum_1-to-5Chr.fasta",
-                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/First_Blaster.csv",
-                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_1/Results100/",
+repetitive_blaster("/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_3/Leishmania_infantum_chr_1-5.fasta",
+                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_3/First_Blaster.csv",
+                   "/home/viskuit/Desktop/Projects/Leishmania/SIDER_Data/Test_3/Results/",
                    "LinJ",
                    600,
                    1,
-                   100                  
+                   2                  
 )
