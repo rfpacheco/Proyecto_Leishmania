b0e7468691968806dd32c785972cea97a1f9b95a
ronnyfph <ronnyfph@gmail.com>
2024-12-09 21:59:31 +0100

    Add progress logging to JSON to CSV conversion script
    Added print statements to provide progress feedback at each step of the conversion process from JSON to CSV. This enhances the user experience by clarifying the program's current status and reducing uncertainty about long-running operations.

--- SIDER_RepetitiveSearcher/extra/correct_coor_json_to_csv.py
+++ SIDER_RepetitiveSearcher/extra/correct_coor_json_to_csv.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
     args = parse_arguments()  # Parse arguments
 
     # Read JSON file containing the dictionary
+    print("1. Loading JSON file...")
     with open(args.file, 'r') as file:
         data_dict = json.load(file)  # With this I have a dictionary python data type
         # Structured of the dictionary
@@ -49,8 +50,10 @@ if __name__ == '__main__':
             # element[2] = start coordinate
             # element[3] = end coordinate
             # element[4] = Accepted or Rejected
+    print("\t DONE")
 
     # Read the negative database
+    print("2. Loading negative database...")
     neg_database = pd.read_csv(args.neg_database, sep=',', header=0)
     # In the negative database
         # element['sseqid'] = chromosome
@@ -58,48 +61,68 @@ if __name__ == '__main__':
         # element['send'] = end coordinate
         # element['sstrand'] = strand
         # element['sseq'] = sequence
+    print("\t DONE")
 
     # Let's start by creating the positive database
+    print("3. Getting accepted or rejected elements...")
     positive_database = [element[0:4] for value in data_dict.values() for element in value if element[4] == 'Accepted']  # Save from chromosome to end coordinate
     negative_database = [element[0:4] for value in data_dict.values() for element in value if element[4] != 'Accepted']
+    print("\t DONE")
 
     # Transform the database to a pandas dataframe
+    print("4. Transforming to data frame...")
     positive_database = pd.DataFrame(positive_database, columns=['sseqid', 'sstrand', 'sstart', 'send'])
     negative_database = pd.DataFrame(negative_database, columns=['sseqid', 'sstrand', 'sstart', 'send'])
+    print("\t DONE")
 
     # Reorder the columns to 'sseqid', 'sstart' , 'send', 'sstrand'
+    print("5. Getting correct order...")
     positive_database = positive_database[['sseqid', 'sstart', 'send', 'sstrand']]
     negative_database = negative_database[['sseqid', 'sstart', 'send', 'sstrand']]
+    print("\t DONE")
 
     # Take the sequence from the genome for both databases
     ## First let's create a blastn dictionary
+    print("6. Creating BLASTn dictionary...")
     path_genome = args.database
     path_blast_dict_folder = os.path.join(args.output_dir, 'blastn_dict')
     os.makedirs(path_blast_dict_folder, exist_ok=True)
     path_blast_dict_file = os.path.join(path_blast_dict_folder, os.path.basename(path_genome))
     blastn_dic(path_genome, path_blast_dict_file)
+    print("\t DONE")
 
     ## Now let's get the sequence for the positive database
+    print("7. Getting positive sequences...")
     positive_database['sseq'] = positive_database.apply(
         lambda x: get_sequence(start_coor=x['sstart'], end_coor=x['send'], strand=x['sstrand'], chromosome=x['sseqid'], path_genome=path_blast_dict_file), axis=1
     )
+    print("\t DONE")
 
     # And do the same for the negative database
+    print("8. Getting negative sequences...")
     negative_database['sseq'] = negative_database.apply(
         lambda x: get_sequence(start_coor=x['sstart'], end_coor=x['send'], strand=x['sstrand'], chromosome=x['sseqid'], path_genome=path_blast_dict_file), axis=1
     )
+    print("\t DONE")
 
     # Add the negative database to the old one
+    print("9. Merging negative databases...")
     negative_database = pd.concat([negative_database, neg_database], ignore_index=True, axis=0)
+    print("\t DONE")
 
     # Make data types conversion just in case
+    print("10. Transforming data...")
     positive_database[['sstart', 'send']] = positive_database[['sstart', 'send']].apply(pd.to_numeric)
     negative_database[['sstart', 'send']] = negative_database[['sstart', 'send']].apply(pd.to_numeric)
+    print("\t DONE")
 
-    # Reorder data 
+    # Reorder data
+    print("11. Reordering data...")
     positive_database.sort_values(by=['sseqid', 'sstart'], inplace=True)
     negative_database.sort_values(by=['sseqid', 'sstart'], inplace=True)
+    print("\t DONE")
 
     # Save the databases
     positive_database.to_csv(os.path.join(args.output_dir, 'positive_database.csv'), sep=',', index=False, header=True)
     negative_database.to_csv(os.path.join(args.output_dir, 'negative_database.csv'), sep=',', index=False, header=True)
+    print("Program finished!")
